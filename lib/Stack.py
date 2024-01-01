73845d0193e42a3de229ce92929f2f8732dfdf5a
Kevin <kevinolongo9@gmail.com>
2024-01-01 10:43:50 +0300

    implement stack methods

--- lib/Stack.py
+++ lib/Stack.py
@@ -1,25 +1,38 @@
 class Stack:
 
-    def __init__(self, items = [], limit = 100):
-        pass
+    def __init__(self, items=None, limit=100):
+        self.items = items if items is not None else []
+        self.limit = limit
 
-    def isEmpty(self):
-        pass
+    def is_empty(self):
+        return len(self.items) == 0
 
     def push(self, item):
-        pass
+        if self.limit is None or len(self.items) < self.limit:
+            self.items.append(item)
+        else:
+            raise OverflowError("Stack is full")
 
     def pop(self):
-        pass
+        if not self.is_empty():
+            return self.items.pop()
+        else:
+            raise IndexError("Pop from an empty stack")
 
     def peek(self):
-        pass
-    
+        if not self.is_empty():
+            return self.items[-1]
+        else:
+            return None
+
     def size(self):
-        pass
+        return len(self.items)
 
     def full(self):
-        pass
+        return self.limit is not None and len(self.items) == self.limit
 
     def search(self, target):
-        pass
+        try:
+            return len(self.items) - self.items[::-1].index(target) - 1
+        except ValueError:
+            return -1
